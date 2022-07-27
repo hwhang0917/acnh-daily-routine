ee9713f5155e2d053056112e0240361e6f169199
hwhang0917 <hwhang0917@gmail.com>
2022-07-28 00:01:45 +0900

    :sparkles: Add SSR props for music

---
+++ interfaces/songList.ts
@@ -0,0 +1,40 @@
+/**
+ * 날씨 종류
+ */
+export type Weather = "Rainy" | "Snowy" | "Sunny";
+export type HourMusicCode = `BGM_24Hour_${string}_${Weather}`;
+
+/**
+ * 시간별 음악
+ * @description GET /v1/backgroundmusic 리턴값
+ */
+export interface HourlyMusic {
+  /**
+   * 음악 고유 ID
+   * @example "BGM_24Hour_00_Rainy"
+   */
+  [key: HourMusicCode]: {
+    /**
+     * 고유 ID
+     */
+    id: number;
+    /**
+     * 파일명
+     */
+    "file-name": string;
+    /**
+     * 시간
+     * @min 0
+     * @max 23
+     */
+    hour: number;
+    /**
+     * 날씨
+     */
+    weather: Weather;
+    /**
+     * 음악 URI
+     */
+    music_uri: string;
+  };
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,36 @@
-import type { NextPage } from "next";
+import type { GetServerSideProps } from "next";
+import axios from "axios";
 import { MusicBox, Sky } from "@components";
+import { HourlyMusic } from "@interfaces";
+import { getSongCode } from "@utils";
 
-const Home: NextPage = () => {
+interface IProps {
+  songList: HourlyMusic;
+}
+
+const Home = ({ songList }: IProps) => {
   return (
     <div>
       <Sky />
       <MusicBox />
+      <h1>
+        {songList[getSongCode({ hour: 23, weather: "Rainy" })]["file-name"]}
+      </h1>
       <h1>Hello World</h1>
     </div>
   );
 };
 
 export default Home;
+
+export const getServerSideProps: GetServerSideProps<IProps> = async () => {
+  const { data: songList } = await axios.get<HourlyMusic>(
+    "https://acnhapi.com/v1/backgroundmusic"
+  );
+
+  return {
+    props: {
+      songList,
+    },
+  };
+};
