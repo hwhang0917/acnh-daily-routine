98e715c13e50c6ea3f876309abfdbef2d0fa4eda
hwhang0917 <hwhang0917@gmail.com>
2022-09-10 20:50:30 +0900

    :lock: Add nextjs security headers

--- next.config.js
+++ next.config.js
@@ -1,7 +1,67 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
+const { PHASE_PRODUCTION_SERVER } = require("next/constants");
 
-module.exports = nextConfig
+const ContentSecurityPolicy = `
+  default-src 'self';
+  script-src 'self';
+  child-src example.com;
+  style-src 'self' example.com;
+  font-src 'self';
+`;
+
+/** @type {Array<{key: string, value: string}>} */
+const securityHeaders = [
+  {
+    key: "X-DNS-Prefetch-Control",
+    value: "on",
+  },
+  {
+    key: "Strict-Transport-Security",
+    value: "max-age=63072000; includeSubDomains; preload",
+  },
+  {
+    key: "X-XSS-Protection",
+    value: "1; mode=block",
+  },
+  {
+    key: "X-Frame-Options",
+    value: "SAMEORIGIN",
+  },
+  {
+    key: "Permissions-Policy",
+    value: "camera=(), microphone=(), geolocation=(), interest-cohort=()",
+  },
+  {
+    key: "X-Content-Type-Options",
+    value: "nosniff",
+  },
+  {
+    key: "Referrer-Policy",
+    value: "origin-when-cross-origin",
+  },
+  {
+    key: "Content-Security-Policy",
+    value: ContentSecurityPolicy.replace(/\s{2,}/g, " ").trim(),
+  },
+];
+
+/** @type {(phase: string) => import('next').NextConfig} */
+module.exports = (phase) => {
+  return {
+    async headers() {
+      return phase === PHASE_PRODUCTION_SERVER
+        ? [
+            {
+              source: "/:path*",
+              headers: securityHeaders,
+            },
+          ]
+        : [];
+    },
+    compiler: {
+      styledComponents: true,
+      removeConsole: true,
+    },
+    reactStrictMode: true,
+    swcMinify: true,
+  };
+};
